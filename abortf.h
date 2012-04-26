bb4886f194804232e370956da2100337e4ed310f
David Holroyd <dave@badgers-in-foil.co.uk>
2012-04-26 21:43:24 +0100

    Provide the function context for the abort too

--- abortf.h
+++ abortf.h
@@ -1,7 +1,7 @@
+#include <stdio.h>
 
+#define ABORTF(format, args...) do { fprintf(stderr, "%s(%s:%d) abort: " format "\n", __func__, __FILE__, __LINE__, ## args); abort(); } while (0)
 
-#define ABORTF(format, args...) do { fprintf(stderr, "%s:%d " format "\n", __FILE__, __LINE__, ## args); abort(); } while (0)
+#define ABORTF_ON(condition, format, args...) do { if (condition) { fprintf(stderr, "%s(%s:%d) abort: " format "\n", __func__, __FILE__, __LINE__, ## args); abort(); } } while (0)
 
-#define ABORTF_ON(condition, format, args...) do { if (condition) { fprintf(stderr, "%s:%d " format "\n", __FILE__, __LINE__, ## args); abort(); } } while (0)
-
-#define ABORT_ON(condition, message) do { if (condition) { fprintf(stderr, "%s:%d %s\n", __FILE__, __LINE__, message); abort(); } } while (0)
+#define ABORT_ON(condition, message) do { if (condition) { fprintf(stderr, "%s(%s:%d) abort: %s\n", __func__, __FILE__, __LINE__, message); abort(); } } while (0)
